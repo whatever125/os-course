5f51b59b4b3f02b1907c74690c7cb5a3e5b52e4d
e1turin <ivturnik2003@gmail.com>
2024-11-25 01:36:35 +0300

    Idiomatic content

--- lab/vtfs/source/vtfs.c
+++ lab/vtfs/source/vtfs.c
@@ -5,18 +5,18 @@
 #define MODULE_NAME "vtfs"
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("vtshniks");
+MODULE_AUTHOR("secs-dev");
 MODULE_DESCRIPTION("A simple FS kernel module");
 
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
 
 static int __init vtfs_init(void) {
-  LOG("VT Flood!\n");
+  LOG("VTFS joined the kernel\n");
   return 0;
 }
 
 static void __exit vtfs_exit(void) {
-  LOG("VT Mute!\n");
+  LOG("VTFS left the kernel\n");
 }
 
 module_init(vtfs_init);
