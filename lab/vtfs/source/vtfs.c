1a9221c4d8e7a28640998945618c0d7eee38823e
Dmitry Kolmakov <kolmakov125@gmail.com>
2025-01-10 08:37:44 -0500

    part 2

--- lab/vtfs/source/vtfs.c
+++ lab/vtfs/source/vtfs.c
@@ -1,4 +1,6 @@
+#include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 
@@ -10,12 +12,66 @@ MODULE_DESCRIPTION("A simple FS kernel module");
 
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
 
+struct inode* vtfs_get_inode(
+  struct super_block* sb, 
+  const struct inode* dir, 
+  umode_t mode, 
+  int i_ino
+) {
+  struct inode *inode = new_inode(sb);
+  if (inode != NULL) {
+    inode_init_owner(inode, dir, mode);
+  }
+
+  inode->i_ino = i_ino;
+  return inode;
+}
+
+int vtfs_fill_super(struct super_block *sb, void *data, int silent) {
+  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR, 1000);
+
+  sb->s_root = d_make_root(inode);
+  if (sb->s_root == NULL) {
+    return -ENOMEM;
+  }
+
+  printk(KERN_INFO "return 0\n");
+  return 0;
+}
+
+struct dentry* vtfs_mount(
+  struct file_system_type* fs_type,
+  int flags,
+  const char* token,
+  void* data
+) {
+  struct dentry* ret = mount_nodev(fs_type, flags, data, vtfs_fill_super);
+  if (ret == NULL) {
+    printk(KERN_ERR "Can't mount file system");
+  } else {
+    printk(KERN_INFO "Mounted successfuly");
+  }
+  return ret;
+}
+
+void vtfs_kill_sb(struct super_block* sb) {
+  printk(KERN_INFO "vtfs super block is destroyed. Unmount successfully.\n");
+}
+
+static struct file_system_type vtfs_fs_type = {
+  .name = "vtfs",
+  .mount = vtfs_mount,
+  .kill_sb = vtfs_kill_sb,
+};
+
 static int __init vtfs_init(void) {
+  register_filesystem(&vtfs_fs_type);
   LOG("VTFS joined the kernel\n");
   return 0;
 }
 
 static void __exit vtfs_exit(void) {
+  unregister_filesystem(&vtfs_fs_type);
   LOG("VTFS left the kernel\n");
 }
 
